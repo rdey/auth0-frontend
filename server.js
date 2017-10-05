5ff3f2cdb1489a283d672bccc4c89ed8d6c15230
Osvaldas Valutis <osvaldas.v@gmail.com>
2017-10-05 21:33:00 +0300

    Init

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+//
+// app.use('/css', express.static(__dirname + '/css/'));
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/html/index.html');
+// });
+//
+// http.listen(3000, function(){
+//   console.log('Server started');
+// });
+
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+var path = require('path');
 
-app.use('/css', express.static(__dirname + '/css/'));
+app.use('/css', express.static(path.join(__dirname + '/css/')));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/html/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/index.html'));
 });
 
-http.listen(3000, function(){
+app.listen(3000, function(){
   console.log('Server started');
 });
